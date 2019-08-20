124e0f2625041b9fe519fb3005e809edeb20f7e6
Abraham Friessen <abraham@pyw.com.mx>
2019-08-19 23:31:44 -0600

    Advanced in Info

--- frontend/src/components/Info.vue
+++ frontend/src/components/Info.vue
@@ -1,19 +1,28 @@
 <template>
   <q-dialog v-model="opened">
     <q-card>
-      Hello
+      <q-toolbar>
+        
+      </q-toolbar>
+      <div class="q-pa-lg">{{ data }}</div>
     </q-card>
   </q-dialog>
 </template>
 
 <script>
+// Imports
+import Axios from 'axios'
 export default {
   data: () => ({
-    opened: false
+    opened: false,
+    data: []
   }),
   created() {
     this.$root.$on("Info", obj => {
       this.opened = true
+      Axios.get('http://localhost:8000/info/').then(res => {
+        this.data = res.data
+      })
     })
   }
 }
