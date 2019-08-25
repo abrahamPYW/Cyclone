232046e02de735aafa4fec7bb1bb5fe0a6a0aa47
Abraham Friessen <abraham@pyw.com.mx>
2019-08-24 18:52:52 -0600

    aded expected

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,10 +1,29 @@
 <template>
-  <q-page class="text-center q-pt-xl" style="padding-top: 25%">
-    <div class="q-pa-md q-gutter-md text-primary">
-      <i class="text-h3">Total:</i><i class="text-h3 text-bold">{{ data.Total | currency }}</i>
+  <q-page>
+    <div class="text-center q-pt-xl" style="padding-top: 5%">
+      <i class="q-pa-md q-gutter-md text-primary">
+        <i class="text-h3">Sponsers:</i>
+        <i class="text-h3 text-bold">{{ expected.sonponsers }}</i>
+      </i>
+      <div class="q-pa-md q-gutter-md text-primary">
+        <i class="text-h3">Total:</i>
+        <i class="text-h3 text-bold">{{ expected.definite | currency }}</i>
+      </div>
+      <i class="q-pa-md q-gutter-md text-primary">
+        <i class="text-h3">Km:</i>
+        <i class="text-h3 text-bold">{{ expected.Km | currency }}</i>
+      </i>
     </div>
-     <div class="q-pa-md q-gutter-md text-primary">
-      <i class="text-h3">Km:</i><i class="text-h3 text-bold">{{ data.Km }}</i>
+    <q-separator style="margin-top: 10%" />
+    <div class="text-center q-pt-xl" style="padding-top: 5%">
+      <div class="q-pa-md q-gutter-md text-primary">
+        <i class="text-h3">Total:</i>
+        <i class="text-h3 text-bold">{{ data.Total | currency }}</i>
+      </div>
+      <div class="q-pa-md q-gutter-md text-primary">
+        <i class="text-h3">Km:</i>
+        <i class="text-h3 text-bold">{{ data.Km }}</i>
+      </div>
     </div>
   </q-page>
 </template>
@@ -13,21 +32,25 @@
 </style>
 
 <script>
-import Axios from 'axios'
+import Axios from "axios";
 export default {
-  name: 'PageHome',
+  name: "PageHome",
   data: () => ({
-    data: {}
+    data: {},
+    expected: {}
   }),
   filters: {
     currency(value) {
-      return '$ ' + value
+      return "$ " + value;
     }
   },
   created() {
     Axios.get("http://localhost:8000/total/").then(res => {
-      this.data = res.data
-    })
+      this.data = res.data;
+    });
+    Axios.get("http://localhost:8000/expected/").then(res => {
+      this.expected = res.data;
+    });
   }
-}
+};
 </script>
