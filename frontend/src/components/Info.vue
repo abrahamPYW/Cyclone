71e2d37bd9ebf0117511c89ea2b8da4fec6f900e
Abraham Friessen <abraham@pyw.com.mx>
2019-08-28 22:50:49 -0600

    added total per participant

--- frontend/src/components/Info.vue
+++ frontend/src/components/Info.vue
@@ -12,9 +12,26 @@
           </tr>
         </thead>
         <tbody>
-          <tr v-for="dat in data" :key="dat.id">
+          <tr v-for="dat in data.sponsers" :key="dat.id">
             <td>{{ dat.sponser }}</td>
-            <td>{{ dat.total }}</td>
+            <td class="text-right">{{ dat.total }}</td>
+          </tr>
+          <tr class="text-bold">
+            <td>Total a Pagar</td>
+            <td class="text-right">{{ '$ ' + data.total }}</td>
+          </tr>
+          <tr class="text-bold">
+            <td>Total Manejado</td>
+            <td class="text-right">{{ data.total_km + ' KM' }}</td>
+          </tr>
+          <tr class="text-bold">
+            <td>Total Precio Fijo</td>
+            <td  class="text-right" v-if="data.definite > 0">{{ data.definite }}</td>
+            <td  class="text-right" v-else>$ 0.00</td>
+          </tr>
+          <tr class="text-bold">
+            <td>Total Precio por KM</td>
+            <td class="text-right">{{ '$ ' + data.price_x_km }}</td>
           </tr>
         </tbody>
       </q-markup-table>
